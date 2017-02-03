59a4677eeaa7d4ca36e0c9038a27d518d2415a11
Tripp Roberts <tripp@tripp-roberts.com>
2017-02-02 21:35:28 -0500

    create scrobbler_users table and make model an ApplicationRecord

--- app/models/scrobbler_user.rb
+++ app/models/scrobbler_user.rb
@@ -1,20 +1,11 @@
-class ScrobblerUser
-
-  attr_accessor :name
-
-  def self.all
-    [ScrobblerUser.new("tripproberts")]
-  end
-
-  def initialize(name)
-    @name = name
-  end
+class ScrobblerUser < ApplicationRecord
 
   def get_top_tracks(limit=5)
-    recent_songs = ScrobblerService.new.get_recent_tracks(name)
+    recent_songs = ScrobblerService.new.get_recent_tracks(username)
     song_listen_count = Hash.new(0)
     recent_songs.each { |song| song_listen_count[song.except("nowplaying", "date")] += 1 }
     ordered_songs = song_listen_count.sort_by(&:last).reverse
     ordered_songs[0,limit]
   end
+
 end
---
+++ db/migrate/20170203022638_create_scrobbler_users.rb
@@ -0,0 +1,9 @@
+class CreateScrobblerUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :scrobbler_users do |t|
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
