b99b9ae41b35e1a7078d5ae6148bafe897221d09
Tripp Roberts <tripp@tripp-roberts.com>
2018-10-02 20:01:07 -0400

    Remove single quotes from track spotify queries

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,7 +2,8 @@ class Track < ApplicationRecord
   has_many :scrobbles
 
   def to_spotify_query
-    album.nil? || album.empty? ? "artist:#{artist} track:#{name}"
-                               : "artist:#{artist} track:#{name} album:#{album}"
+    base = album.nil? || album.empty? ? "artist:#{artist} track:#{name}"
+                                      : "artist:#{artist} track:#{name} album:#{album}"
+    base.gsub("'","")
   end
 end
